9fcc7b9b1a1ba71a2d6d2f13fc3d190c30c54bb6
David Dollar <ddollar@gmail.com>
2012-01-23 10:36:29 -0500

    try new tat

--- Library/Formula/tat.rb
+++ Library/Formula/tat.rb
@@ -1,16 +1,13 @@
 require 'formula'
 
 class Tat < Formula
-  url 'https://github.com/ryandotsmith/tat/tarball/e6d8043a24f786c885d1df9954d21db98b39e530'
-  version 'e6d8043a24f786c885d1df9954d21db98b39e530'
+  url 'https://github.com/ddollar/tat/tarball/15c8a30ae4aee265f0d59f0b5766ca8618481db5'
+  version '15c8a30ae4aee265f0d59f0b5766ca8618481db5'
   homepage 'https://github.com/ryandotsmith/tat'
-  head 'https://github.com/ryandotsmith/tat.git'
-  md5 'f70aa226d1cb033a42483e99fcf42f1b'
+  head 'https://github.com/ddollar/tat.git'
+  md5 'cf2ee8ada24df24975f3bd804db8bc56'
 
   def install
-    cp "tat.sh", prefix+"tat.sh"
-    system "chmod u+x #{prefix+"tat.sh"}"
-    bin.mkpath
-    ln_s prefix+"tat.sh", bin+"tat"
+    system "make", "install", "prefix=#{prefix}"
   end
 end
