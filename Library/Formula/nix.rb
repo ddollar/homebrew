b0d2cec6d705c257cf731806760f40472be14da9
David Dollar <ddollar@gmail.com>
2012-01-26 13:02:53 -0800

    nix

---
+++ Library/Formula/nix.rb
@@ -0,0 +1,15 @@
+require 'formula'
+
+class Nix < Formula
+  url 'http://hydra.nixos.org/build/565033/download/5/nix-0.16.tar.gz'
+  homepage 'http://nixos.org/nix/'
+  md5 'f214607a8ab8d1e37acfe6d5023feec4'
+
+  skip_clean %w( bin lib )
+
+  def install
+    system "./configure", "--prefix=#{prefix}",
+      "--with-bzip2=/usr/bin", "--localstatedir=#{var}/nix"
+    system "make install"
+  end
+end
